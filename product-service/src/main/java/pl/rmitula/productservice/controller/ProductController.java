6a20bc83c7577786e92a28aa381cf8e1a68d34c0
rmitula <rmitula@gmail.com>
2018-07-23 21:45:52 +0200

    prepare for gateway-service

--- product-service/src/main/java/pl/rmitula/productservice/controller/ProductController.java
+++ product-service/src/main/java/pl/rmitula/productservice/controller/ProductController.java
@@ -2,18 +2,15 @@ package pl.rmitula.productservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.rmitula.productservice.model.Product;
 import pl.rmitula.productservice.properties.ProductProperties;
 import pl.rmitula.productservice.service.ProductService;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Logger;
 
 @RestController
-@RequestMapping("/product")
 public class ProductController {
 
     private static final Logger LOG = Logger.getLogger(ProductController.class.getName());
